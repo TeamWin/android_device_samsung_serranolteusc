09f69ed7e51d9dd6ee5fce7e84d1f91d0a0f4b67
jlask <jason972000@yahoo.com>
2014-11-04 20:27:42 -0600

    device files for serranolteusc

---
+++ extract-files.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+export DEVICE=serranolteusc
+export VENDOR=samsung
+
+set -e
+
+if [ $# -eq 0 ]; then
+  SRC=adb
+else
+  if [ $# -eq 1 ]; then
+    SRC=$1
+  else
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
+  fi
+fi
+
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+rm -rf $BASE/*
+
+echo "Extracting to $BASE"
+
+echo
+
+echo "Using device-proprietary-files.txt"
+for FILE in `egrep -v '(^#|^$)' ./device-proprietary-files.txt`; do
+  echo "Extracting /system/$FILE ..."
+  DIR=`dirname $FILE`
+  if [ ! -d $BASE/$DIR ]; then
+    mkdir -p $BASE/$DIR
+  fi
+  if [ "$SRC" = "adb" ]; then
+    adb pull /system/$FILE $BASE/$FILE
+  else
+    cp $SRC/system/$FILE $BASE/$FILE
+  fi
+done
+
+echo
+
+echo "Using proprietary-files.txt"
+for FILE in `egrep -v '(^#|^$)' ./proprietary-files.txt`; do
+  echo "Extracting /system/$FILE ..."
+  DIR=`dirname $FILE`
+  if [ ! -d $BASE/$DIR ]; then
+    mkdir -p $BASE/$DIR
+  fi
+  if [ "$SRC" = "adb" ]; then
+    adb pull /system/$FILE $BASE/$FILE
+  else
+    cp $SRC/system/$FILE $BASE/$FILE
+  fi
+done
+
+echo
+
+echo "Using common-proprietary-files.txt"
+#rm -rf $BASE/*
+for FILE in `egrep -v '(^#|^$)' ./common-proprietary-files.txt`; do
+  echo "Extracting /system/$FILE ..."
+  DIR=`dirname $FILE`
+  if [ ! -d $BASE/$DIR ]; then
+    mkdir -p $BASE/$DIR
+  fi
+  if [ "$SRC" = "adb" ]; then
+    adb pull /system/$FILE $BASE/$FILE
+  else
+    cp $SRC/system/$FILE $BASE/$FILE
+  fi
+done
+
+./setup-makefiles.sh
