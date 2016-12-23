9a9c1820d9717f656998241c3eef107fa69e3196
Jason Laskowski <jason972000@gmail.com>
2016-12-23 04:55:04 -0600

    fix derp

--- cm.mk
+++ cm.mk
@@ -15,7 +15,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from serranoltexx device
+# Inherit from serranolteusc device
 $(call inherit-product, device/samsung/serranolteusc/device.mk)
 
 # Inherit some common CM stuff.
